c358534cf196c7ff99f62c79bcb03c2c9e87b307
Jon Pugh <jon@thinkdrop.net>
2013-11-05 16:07:39 -0500

    Adding project page.

--- src/HubDrop/Bundle/Controller/HubDropController.php
+++ src/HubDrop/Bundle/Controller/HubDropController.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HubDropController extends Controller
 {
-    public function homeAction()
-    {
-        /*
-         * The action's view can be rendered using render() method
-         * or @Template annotation as demonstrated in DemoController.
-         *
-         */
-        return $this->render('HubDropBundle:HubDrop:home.html.twig');
-    }
+  /**
+   * Homepage
+   */
+  public function homeAction()
+  {
+    // @TODO: Goto Project form
+    return $this->render('HubDropBundle:HubDrop:home.html.twig');
+  }
+
+  /**
+   * Project View Page
+   */
+  public function projectAction($project_name)
+  {
+    /*
+     * The action's view can be rendered using render() method
+     * or @Template annotation as demonstrated in DemoController.
+     *
+     */
+    $params['project_name'] = $project_name;
+    $params['project_drupal_url'] = "http://drupal.org/project/$project_name";
+    $params['project_drupal_git'] = "http://git.drupal.org/project/$project_name";
+    return $this->render('HubDropBundle:HubDrop:project.html.twig', $params);
+  }
 }
