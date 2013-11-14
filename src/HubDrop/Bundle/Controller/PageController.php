eaa1d00811f0f5f9dfe54a7ed83b5405c83f407f
Jon Pugh <jon@thinkdrop.net>
2013-11-14 11:16:00 -0500

    adding twitter stuff and improving about page.

--- src/HubDrop/Bundle/Controller/PageController.php
+++ src/HubDrop/Bundle/Controller/PageController.php
@@ -12,8 +12,31 @@ class PageController extends Controller
    */
   public function aboutAction()
   {
-    //
+    $content = <<<HTML
+<h2 class="text-center">About</h2>
+<div class='col-sm-6'>
+<p>
+  <a href='http://hubdrop.io'>HubDrop.io</a> provides mirroring of <a href='http://hubdrop.io'>drupal.org</a> projects on <a href='http://github.com'>GitHub</a> as a free public service.
+</p>
+<p>
+  Drupal.org's git hosting or issue tracking will not be able to move to GitHub anytime soon, so hubdrop will act as a bridge for developers who wish to leverage GitHub for their projects, yet stay connected with the drupal.org community.
+</p>
+</div>
+<div class='col-sm-6'>
+<p>
+  HubDrop itself is <a href='http://github.com/hubdrop'>fully open source</a> and hosted on GitHub, so anyone can fork and improve the code. Hubdrop code includes <a href='http://opsworks.com'>Chef recipes</a> and a <a href='http://vagrantup.com'>Vagrantfile</a>, making deployment and development easy.
+</p>
+<p>
+  Plans for the future include allowing projects to move to GitHub as the primary repo, with mirroring or releases being sent back to drupal.org.
+</p>
+</div>
+<div class='col-sm-12 text-center text-muted'>
+<p>
+  HubDrop.io is brought to you by <a href='http://thinkdrop.net'>THINKDROP</a>.
+</p>
+</div>
+HTML;
     return $this->render('HubDropBundle:HubDrop:page.html.twig', array(
-      'content' => 'welcome'));
+      'content' => $content));
   }
 }
