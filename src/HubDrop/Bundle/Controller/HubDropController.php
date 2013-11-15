26ce47c8e3db9b6584895763372fa4ffbf3eb178
Jon Pugh <jon@thinkdrop.net>
2013-11-15 17:45:50 -0500

    changing org name again.

--- src/HubDrop/Bundle/Controller/HubDropController.php
+++ src/HubDrop/Bundle/Controller/HubDropController.php
@@ -11,7 +11,7 @@ use Github\Client as GithubClient;
 class HubDropController extends Controller
 {
   private $repo_path = '/var/hubdrop/repos';
-  private $github_org = 'drupal-projects';
+  private $github_org = 'drupal-project';
   private $jenkins_url = 'http://hubdrop:8080';
 
   /**
