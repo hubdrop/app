82479545bceef72155ed8fc6f4ff8911998efd22
Jon Pugh <jon@thinkdrop.net>
2013-11-16 13:42:56 -0500

    removing bad debug code

--- src/HubDrop/Bundle/Controller/HubDropController.php
+++ src/HubDrop/Bundle/Controller/HubDropController.php
@@ -149,9 +149,7 @@ class HubDropController extends Controller
     if (!$stop_process) {
       $output = shell_exec('jenkins-cli build hubdrop-jenkins-create-mirror -p NAME=' . $project_name);
     }
-    print $output;
-    die;
     //return new Response($output);
-    //return $this->redirect('/project/' . $project_name);
+    return $this->redirect('/project/' . $project_name);
   }
 }
