565750d01be8cc9440f5cbc0395c7f4082467cf2
Jon Pugh <jon@thinkdrop.net>
2013-12-08 13:09:51 -0500

    Fixing up project page to use new service!

--- src/HubDrop/Bundle/Controller/HubDropController.php
+++ src/HubDrop/Bundle/Controller/HubDropController.php
@@ -12,11 +12,6 @@ class HubDropController extends Controller
   private $github_org = 'drupalprojects';
   private $jenkins_url = 'http://hubdrop:8080';
 
-  /**
-   * This was retrieved using a github username and password with curl:
-   * curl -i -u <github_username> -d '{"scopes": ["repo"]}' https://api.github.com/authorizations
-   */
-  private $github_application_token = '2f3a787bc2881ac86d0277b37c1b9a67c4c509bb';
 
   /**
    * Route: Homepage
@@ -40,86 +35,35 @@ class HubDropController extends Controller
    */
   public function projectAction($project_name)
   {
-      //$mailer = $this->get('my_mailer');
 
-      // Loading a project
-      $project = $this->get('hubdrop')->getProject($project_name);
+    // @TODO: Break out into its own route. Require a POST? Symfony Form API?
+    // Mirror: GO!
+    // We only want to try to mirror a project if not yet cloned and it exists.
+    if ($this->get('request')->query->get('mirror') == 'go'){
+      return $this->mirrorProject($project_name);
+    }
 
-      // Ideal?
-      //$project = $this->get('hubdrop')->project->get($project_name);
+    // Get Project object.
+    $project = $this->get('hubdrop')->getProject($project_name);
 
-
-      // Default twig variables
     $params = array();
-    $params['project_cloned'] = FALSE;    // Cloned Locally
-    $params['project_mirrored'] = FALSE;  // On GitHub
-    $params['project_exists'] = FALSE;    // On drupal.org
-    $params['message'] = '';
-
-    $params['github_web_url'] = $project->github_http_url;
-    $params['local_clone_path'] = $project->local_clone_path;
-
-    // @TODO: Figure out how to cache this, or rig it up so it is faster.
-
-    // Check Steps:
-    //  1. Check for local path.
-    //  2. Check for Public github repo, if local path is missing. (This is
-    //       really just for easy local development.  Allows the web app to work
-    //       without having all the repos cloned.
-    //  3. Check for drupal.org project.
-
-    // If we have a local clone...
-    if (file_exists($params['local_clone_path'])){
-      $params['project_cloned'] = TRUE;
-      $params['project_mirrored'] = TRUE;
-      $params['project_exists'] = TRUE;
-      // We will assume it exists on github if we have a local clone, to save
-      // the extra request to github.
-    }
-    // If we don't have a local clone, ping github to see if it is already mirrored.
-    else {
-      $params['project_cloned'] = FALSE;
-
-      // Lookup GitHub project.
-      // @TODO: Should we only do this in dev environments?
-      $client = new Client("http://github.com");
-      try {
-        $response = $client->get('/' . $this->github_org . '/' . $project_name)->send();
-        $params['project_mirrored'] = TRUE;
-        $params['project_exists'] = TRUE;
-
-      } catch (\Guzzle\Http\Exception\BadResponseException $e) {
-        $params['project_mirrored'] = FALSE;
-      }
-    }
-
-    // If project is not yet mirrored, confirm it is really a drupal project.
-    if ($params['project_mirrored'] == FALSE) {
-      // Look for drupal.org/project/{project_name}
-      $client = new Client('http://drupal.org');
-      try {
-        $response = $client->get('/project/' . $project_name)->send();
-        $params['project_exists'] = TRUE;
-
-        // @TODO: Break out into its own route. Require a POST? Symfony Form API?
-        // Mirror: GO!
-        // We only want to try to mirror a project if not yet cloned and it exists.
-        if ($this->get('request')->query->get('mirror') == 'go'){
-          return $this->mirrorProject($project_name);
-        }
-
-      } catch (\Guzzle\Http\Exception\BadResponseException $e) {
-        $params['project_exists'] = FALSE;
-      }
-    }
-
-    // Build template params
     $params['project_name'] = $project_name;
-    $params['project_drupal_url'] = $project->drupal_http_url;
+    $params['project_exists'] = $project->checkUrl('drupal', 'web');    // On drupal.org
+    $params['urls'] = $project->urls;
 
     if ($params['project_exists']){
-      $params['project_drupal_git'] = "http://git.drupal.org/project/$project_name.git";
+      $params['project_cloned'] = $project->checkUrl('local', 'file');    // Cloned Locally
+      $params['project_mirrored'] = $project->checkUrl('github', 'web');  // On GitHub
+      $params['message'] = '';
+
+      $params['urls'] = $project->urls;
+      $params['project_drupal_git'] = $project->getUrl('drupal', 'web');
     }
+
+    // Stopgap
+    $params['message'] = 'This branch is in development.  Mirroring is temporarily disabled.';
+    $params['allow_mirroring'] = FALSE;
+
     return $this->render('HubDropBundle:HubDrop:project.html.twig', $params);
   }
 
@@ -128,33 +72,33 @@ class HubDropController extends Controller
    */
   private function mirrorProject($project_name)
   {
-    $stop_process = FALSE;
-
-    // Connect to GitHub and create a Repo for $project_name
-    // From https://github.com/KnpLabs/php-github-api/blob/master/doc/repos.md
-    $client = new GithubClient();
-    $client->authenticate($this->github_application_token, '', \Github\Client::AUTH_URL_TOKEN);
-
-    try {
-      $repo = $client->api('repo')->create($project_name, "Mirror of http://drupal.org/project/$project_name provided by hubdrop.", "http://hubdrop.io", true, $this->github_org);
-      $output = "GitHub Repo created at " . $repo['html_url'];
-    }
-    catch (\Github\Exception\ValidationFailedException $e) {
-      // For now we assume this is just a
-      // "Validation Failed: name already exists on this account
-      if ($e->getCode() == 422){
-        $output = '<p>Repo already exists on github: http://github.com/' . $this->github_org . '/' . $project_name . '</p>';
-      }
-      else {
-        $output = $e->getMessage();
-        $stop_process = TRUE;
-      }
-    }
-
-    if (!$stop_process) {
-      $output = shell_exec('jenkins-cli build hubdrop-jenkins-create-mirror -p NAME=' . $project_name);
-    }
-    //return new Response($output);
-    return $this->redirect('/project/' . $project_name);
+//    $stop_process = FALSE;
+//
+//    // Connect to GitHub and create a Repo for $project_name
+//    // From https://github.com/KnpLabs/php-github-api/blob/master/doc/repos.md
+//    $client = new GithubClient();
+//    $client->authenticate($this->github_application_token, '', \Github\Client::AUTH_URL_TOKEN);
+//
+//    try {
+//      $repo = $client->api('repo')->create($project_name, "Mirror of http://drupal.org/project/$project_name provided by hubdrop.", "http://hubdrop.io", true, $this->github_org);
+//      $output = "GitHub Repo created at " . $repo['html_url'];
+//    }
+//    catch (\Github\Exception\ValidationFailedException $e) {
+//      // For now we assume this is just a
+//      // "Validation Failed: name already exists on this account
+//      if ($e->getCode() == 422){
+//        $output = '<p>Repo already exists on github: http://github.com/' . $this->github_org . '/' . $project_name . '</p>';
+//      }
+//      else {
+//        $output = $e->getMessage();
+//        $stop_process = TRUE;
+//      }
+//    }
+//
+//    if (!$stop_process) {
+//      $output = shell_exec('jenkins-cli build hubdrop-jenkins-create-mirror -p NAME=' . $project_name);
+//    }
+//    //return new Response($output);
+//    return $this->redirect('/project/' . $project_name);
   }
 }
