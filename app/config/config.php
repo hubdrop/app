d66ffae69cff4de6a264de69786376ca5175b583
Jon Pugh <jon@thinkdrop.net>
2014-02-13 10:57:32 -0500

    ADding hubdrop.yml

--- app/config/config.php
+++ app/config/config.php
@@ -6,22 +6,19 @@
  */
 
 // Check for the file
-if (file_exists('/etc/hubdrop-github-authorization')) {
+if (file_exists('~/.github-authorization')) {
 
   // Load key and set parameter.
-  $key = file_get_contents('/etc/hubdrop-github-authorization');
+  $key = file_get_contents('~/.github-authorization');
   $container->setParameter('app.github.authorization', $key);
 }
 // If file doesn't exist, and not running the github_auth command, throw exception.
 else {
-
-  // Check for console command
-  $input = new \Symfony\Component\Console\Input\ArgvInput();
-  $is_auth_command = $input->getFirstArgument() == 'hubdrop:github_auth';
-
-  if (!$is_auth_command){
-    throw new Exception('Missing github authorization. Run `sudo hubdrop github_auth username` to generate one and write it to /etc/hubdrop-github-authorization.');
-  }
+  $container->setParameter('app.github.authorization', '');
+//
+//  if (!$is_auth_command){
+//    throw new Exception('Missing github authorization. Run `hubdrop github_auth username` to generate one and write it to ~/.github-authorization.');
+//  }
 }
 
 
