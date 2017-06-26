23676c5f49f3abcd5e8d5e282ce0d8b8eb34d866
Jon Pugh <jon@thinkdrop.net>
2017-06-26 12:48:08 -0400

    Pass SYMFONY_ENV from Docker environment to apache environment using /etc/apache2/envvars

--- docker/hubdrop-entrypoint.sh
+++ docker/hubdrop-entrypoint.sh
@@ -9,6 +9,10 @@ echo "HD || Running setfacl -dR -m u:www-data:rwX -m u:hubdrop:rwX /var/hubdrop/
 setfacl -dR -m u:www-data:rwX -m u:hubdrop:rwX /var/hubdrop/app/app/cache /var/hubdrop/app/app/logs
 
 # Launch apache2-foreground in a new process
+echo "HD || Saving SYMFONY_ENV to /etc/apache2/envvars from environment variables ..."
+echo "export SYMFONY_ENV=$SYMFONY_ENV"
+echo "export SYMFONY_ENV=$SYMFONY_ENV" >> /etc/apache2/envvars
+
 echo "HD || Running apache2-foreground& ..."
 sudo apache2-foreground&
 
