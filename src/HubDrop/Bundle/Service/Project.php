ac967c4c456103fc4aa78d9c8be7dda407662777
Jon Pugh <jon@thinkdrop.net>
2017-06-28 16:39:20 -0400

    Get the current URL.

--- src/HubDrop/Bundle/Service/Project.php
+++ src/HubDrop/Bundle/Service/Project.php
@@ -619,7 +619,7 @@ class Project {
     // The project page, hopefully
     $link = $mink->getSession()->getPage()->findLink('Maintainers');
     if (!$link) {
-      throw new \Exception('Unable to access project maintainers list. Add "hubdrop" to the project, allowing Write to VCS and Administer Maintainers. URL: ' . $this->getUrl());
+      throw new \Exception('Unable to access project maintainers list. Add "hubdrop" to the project, allowing Write to VCS and Administer Maintainers. URL: ' . $mink->getSession()->getCurrentUrl());
     }
 
     // Click "Maintainers"
